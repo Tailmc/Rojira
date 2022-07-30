05b40f9790f5e3bb4784a5482010d47cdaa40c39
Tail <95581499+Tailmc@users.noreply.github.com>
2022-07-30 09:51:09 +0900

    Added /Rojira search command

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const http = require('http')
-const { Client, Intents, MessageActionRow, MessageEmbed, MessageButton } = require('discord.js');
+const { Client, Intents, MessageActionRow, MessageEmbed, MessageButton, Interaction } = require('discord.js');
 const { default: axios } = require('axios')
 const fs = require('fs')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
@@ -12,6 +12,7 @@ const options =
     whileControl: (oldPage, newPage) => oldPage === newPage,
     lapse: 5000
 }
+let key;
 
 webmonitoring.monitor('https://bugs.mojang.com/rest/api/latest/search?jql=project=MC&issuetype=Bug&maxResults=1', options)
     .start()
@@ -22,7 +23,7 @@ webmonitoring.monitor('https://bugs.mojang.com/rest/api/latest/search?jql=projec
         const mod = /optifine|forge|fabric|mods|paper|spigot|bukkit/.test(res.data.issues[0].fields.description.toLowerCase())
         const translation = /chinese|spanish|hindi|arabic|portuguese|russian|japanese|french|german|italian|translation/.test(res.data.issues[0].fields.description.toLowerCase())
         const old = /1.8.9|1.12.2|1.13.2|1.14.4|1.15.2|1.16.5|1.17.1|1.18.2/.test(res.data.issues[0].fields.description.toLowerCase())
-        const language = detector.detect(res.data.issues[0].fields.summary)
+        const language = detector.detect(res.data.issues[0].fields.summary + " " + res.data.issues[0].fields.description.split("\n")[0])
         const embedarray = []
         const links = new MessageActionRow()
             .addComponents(
@@ -31,7 +32,7 @@ webmonitoring.monitor('https://bugs.mojang.com/rest/api/latest/search?jql=projec
                     .setLabel('Main')
                     .setStyle('LINK')
             )
-        if (fs.readFileSync('id.txt', 'utf-8') === res.data.issues[0].key) return
+        if (key === res.data.issues[0].key) return
         if (language.length && language[0][0] !== "english") {
             const embed_language = new MessageEmbed()
                 .setColor('RED')
@@ -70,15 +71,93 @@ webmonitoring.monitor('https://bugs.mojang.com/rest/api/latest/search?jql=projec
             )
         }
         await channel.send({ content: res.data.issues[0].key + ": " + res.data.issues[0].fields.summary, embeds: embedarray, components: [links] })
-        fs.writeFileSync('id.txt', res.data.issues[0].key)
+        key = res.data.issues[0].key
     })
 
-client.on('ready', () => {
+client.on('ready', async () => {
     console.log('reloaded');
     client.user.setActivity('Mojira', {
         type: 'WATCHING',
     });
+    await client.application.commands.set([
+        {
+            name: "rojira",
+            description: "get information about Mojira",
+            options: [{
+                type: "SUB_COMMAND",
+                name: "search",
+                description: "search issues in Mojira",
+                options: [{
+                    type: "STRING",
+                    name: "query",
+                    description: "words to search",
+                    required: true,
+                }]
+            }]
+        }
+    ])
 });
+
+client.on('interactionCreate', async (interaction) => {
+    let page = 0;
+    if (!interaction.isCommand()) return;
+    if (interaction.commandName === "rojira") {
+        if (interaction.options.getSubcommand() === "search") {
+            await interaction.deferReply()
+            const sch = await axios.get(`https://bugs.mojang.com/rest/api/latest/search?jql=text~"${encodeURI(interaction.options.getString('query'))}"&issuetype=Bug&maxResults=25`)
+            function getresults(startvalue) {
+                const fields = []
+                for (let i = startvalue; i < startvalue + 5; i++) {
+                    if (sch.data.issues[i]) {
+                        fields.push({ name: sch.data.issues[i].key, value: `[${sch.data.issues[i].fields.summary}](https://bugs.mojang.com/browse/${sch.data.issues[i].key})`, inline: false })
+                    }
+                }
+                embed.setFields(fields)
+                embed.setFooter({text: page +1 + "/5"})
+            }
+            const embed = new MessageEmbed()
+                .setTitle('View full results on bugs.mojang.com')
+                .setURL(`https://bugs.mojang.com/issues/?jql=text~"${encodeURI(interaction.options.getString('query'))}"`)
+                .setAuthor({ name: `The command was triggerd by ${interaction.user.username}`, iconURL: `${interaction.user.displayAvatarURL()}` });
+            const arrows = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setCustomId('backward')
+                        .setEmoji('⏪')
+                        .setStyle('SECONDARY')
+                ).addComponents(
+                    new MessageButton()
+                        .setCustomId('forward')
+                        .setEmoji('⏩')
+                        .setStyle('SECONDARY')
+                )
+
+            getresults(page)
+            await interaction.editReply({ content: `**Results**`, embeds: [embed], components: [arrows] })
+
+            const filter = (i) => i.user.id === interaction.user.id
+
+            const collector = interaction.channel.createMessageComponentCollector({ filter, time: 1000 * 60 * 3 });
+
+            collector.on('collect', async (i) => {
+                if (i.customId === "backward") {
+                    if (page > 0) {
+                        page = page - 1
+                    }
+                    getresults(5 * page)
+                    await i.update({ content: `**Results**`, embeds: [embed] })
+                } else if (i.customId === "forward") {
+                    if (page < 4) {
+                        page = page + 1
+                    }
+                    getresults(5 * page)
+                    await i.update({ content: `**Results**`, embeds: [embed]})
+                }
+            })
+        }
+    }
+
+})
 
 if (process.env.TOKEN == undefined) {
     console.log("undefined token");
